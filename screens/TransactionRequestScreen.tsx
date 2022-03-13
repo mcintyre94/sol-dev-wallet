6700bbd694f4f68dfff7c49125c31e2332cbcf8f
Callum McIntyre <callum@pointer.gg>
2022-03-13 20:53:15 +0000

    Add logic to fetch transaction and initial render, no transaction logic yet

--- screens/TransactionRequestScreen.tsx
+++ screens/TransactionRequestScreen.tsx
@@ -17,15 +17,31 @@ interface FetchedDetails extends TransactionRequestStatus {
   icon: string
 }
 
-interface TransactionsGetResponse {
+interface FetchingTransaction extends TransactionRequestStatus {
+  type: 'FetchingTransaction',
+  label: string,
+  icon: string
+}
+
+interface FetchedTransaction extends TransactionRequestStatus {
+  type: 'FetchedTransaction',
+  transaction: string,
+  label: string,
+  icon: string,
+  message?: string
+}
+
+interface TransactionDetailsResponse {
   label: string,
   icon: string,
 }
 
-interface TransactionPostResponse {
+interface TransactionResponse {
   transaction: string,
   message?: string,
 }
+
+const publicKey = 'Fkc4FN7PPhyGsAcHPW3dBBJ4BvtYkDr2rBFBgFpvy3nB';
 
 export default function TransactionRequestScreen({ route }) {
   const [status, setStatus] = useState(FetchingDetails);
@@ -36,7 +52,7 @@ export default function TransactionRequestScreen({ route }) {
 
   async function fetchDetails() {
     const response = await axios.get(decodedUrl);
-    const { label, icon } = await response.data as TransactionsGetResponse;
+    const { label, icon } = await response.data as TransactionDetailsResponse;
     setStatus({ type: 'FetchedDetails', label, icon } as FetchedDetails);
   }
 
@@ -44,6 +60,18 @@ export default function TransactionRequestScreen({ route }) {
     fetchDetails();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  async function fetchTransaction(fetchedDetails: FetchedDetails) {
+    setStatus({ type: 'FetchingTransaction', label: fetchedDetails.label, icon: fetchedDetails.icon } as FetchingTransaction);
+
+    const response = await axios.post(decodedUrl, {
+      account: publicKey,
+    });
+
+    const { transaction, message } = await response.data as TransactionResponse;
+
+    setStatus({ type: 'FetchedTransaction', transaction, label: fetchedDetails.label, icon: fetchedDetails.icon, message } as FetchedTransaction);
+  }
 
   if (status.type === 'FetchingDetails') {
     return (
@@ -56,7 +84,6 @@ export default function TransactionRequestScreen({ route }) {
 
   if (status.type === 'FetchedDetails') {
     const fetchedDetails = status as FetchedDetails;
-    console.log({ fetchedDetails });
 
     return (
       <View style={styles.container}>
@@ -67,13 +94,52 @@ export default function TransactionRequestScreen({ route }) {
         <View style={styles.bottomView}>
           <Button
             title="Fetch Transaction"
-          // onPress={() => fetchTransaction(fetchedDetails)}
+            onPress={() => fetchTransaction(fetchedDetails)}
           />
           <Text>This will send your public key to {hostname}</Text>
         </View>
       </View>
     );
   }
+
+  if (status.type === 'FetchingTransaction') {
+    const fetchedDetails = status as FetchingTransaction;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.mainView}>
+          <ImageUri uri={fetchedDetails.icon} height={200} width={200} />
+          <Text style={styles.titleText}>{fetchedDetails.label}</Text>
+        </View>
+        <View style={styles.bottomView}>
+          <ActivityIndicator />
+          <Text>This will send your public key to {hostname}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  if (status.type === 'FetchedTransaction') {
+    const fetchedTransaction = status as FetchedTransaction;
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.mainView}>
+          <ImageUri uri={fetchedTransaction.icon} height={200} width={200} />
+          <Text style={styles.titleText}>{fetchedTransaction.label}</Text>
+          {fetchedTransaction.message && <Text>{fetchedTransaction.message}</Text>}
+          <Text>{fetchedTransaction.transaction}</Text>
+        </View>
+        <View style={styles.bottomView}>
+          <Button
+            title="Confirm Transaction"
+          // onPress={() => fetchTransaction(fetchedDetails)}
+          />
+        </View>
+      </View>
+    )
+  }
+
 
   return (
     <View style={styles.container}>
